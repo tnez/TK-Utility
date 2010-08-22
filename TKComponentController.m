09e78ae244c32f196c5d8f0aa3593d3ec54748e6
Travis Nesland <tnesland@gmail.com>
2010-08-22 12:27:07 -0400

    Clock: 20100822 0900-1230

--- TKComponentController.m
+++ TKComponentController.m
@@ -17,67 +17,100 @@
 
 
 @implementation TKComponentController
-@synthesize componentStartTime,componentEndTime,componentType,definition,delegate,view;
+@synthesize delegate,dataDirectory,view,sessionWindow,subject,componentStartTime,componentEndTime;
 
 -(void) begin {
-    [self loadView];
+    switch ([definition valueForKey:TKComponentTypeKey]) {
+        case TKComponentBundleType:
+            // load nib resource
+            [NSBundle loadNibNamed:[definition valueForKey:TKComponentViewNibNameKey] owner:self];
+            [self loadView];
+            break;
+        case TKComponentCocoaAppType:
+            // TODO: load cocoa application
+            [self throwError:@"Cannot load CoCoa applications (support is coming soon)" andBreak:YES];
+            break;
+        case TKComponentFutureBasicAppType:
+            // TODO: load future basic application
+            [self throwError:@"Cannot load Future Basic applications (support is coming soon)" andBreak:YES];
+            break;
+        default:
+            [self throwError:@"Specified component type is not recognized" andBreak:YES];
+    }
     componentStartTime = current_time_marker();
-}
-
--(void) closeView {
-	// ... nothing needed as of now
+    [[NSNotificationCenter defaultCenter] postNotificationName:TKComponentDidBeginNotification object:self];
 }
 
 -(void) dealloc {
+    [dataDirectory release];
     [definition release];
-    [componentType release];
 	[super dealloc];
 }
 
 -(void) end {
     componentEndTime = current_time_marker();
     [view exitFullScreenModeWithOptions:nil];
-    if([delegate respondsToSelector:@selector(componentDidFinish:)]) {
-        [delegate componentDidFinish:self];
-    }
+    [[NSNotificationCenter defaultCenter] postNotificationName:TKComponentDidFinishNotification object:self];
 }
 
-+(id) loadFromDefinition:(NSDictionary *) newDefinition sender:(id) sender {
+-(BOOL) isClearedToBegin {
+    BOOL exists, isDirectory;
+    exists = [[NSFileManager defaultManager] fileExistsAtPath:dataDirectory isDirectory:&isDirectory];
+    if(exists) {
+        if(isDirectory) {
+            return YES; // expected case (file exists and is directory)
+        } else {
+            [self throwError:@"Data Directory is not valid" andBreak:YES];
+            return NO;
+        }
+    } else {
+        // try to create directory
+        NSError *creationError = nil;
+        [[NSFileManager defaultManager] createDirectoryAtPath:dataDirectory withIntermediateDirectories:YES attributes:nil error:&creationError];
+        if(creationError) { // there was an error creating the directory
+            [self throwError:@"Could not create Data Directory" andBreak:YES];
+            return NO;
+        } else { // there was no error creating the directory
+            return YES;
+        }
+    }
+}
+                
++(id) loadFromDefinition:(NSDictionary *) newDefinition {
     // create new instance
     TKComponentController *newComponent = [[TKComponentController alloc] init];
     // configure new component
-    [newComponent loadDefinition:newDefinition];
-    [newComponent setDelegate:sender];
-    [NSBundle loadNibNamed:[newDefinition valueForKey:TK_COMPONENT_NIB_FILE_NAME_KEY] owner:newComponent];
+    [newComponent setDefinition:newDefinition];
     return [newComponent autorelease];
 }
 
--(void) loadDefinition:(NSDictionary *) newDefinition {
-    [self setDefinition:newDefinition];
-}
-    
 -(void) loadView {
-	if([delegate respondsToSelector:@selector(window)]) {
-		[[TKLibrary sharedLibrary] centerView:view inWindow:[delegate window]];
-	} else {
-        [self throwError:@"Could not load view" andBreak:YES];
-	}
+    [[TKLibrary sharedLibrary] centerView:view inWindow:sessionWindow];
 }
 
--(void) loadPreferences {
-    // each sub-class should implement a loadPreferences function and
-    // call [super loadPreferences] before closing to pass on up the chain
-    [self setComponentType:[definition valueForKey:TK_COMPONENT_TYPE_KEY]];
-    // this is the end of the chain
+- (void)setDefinition: (NSDictionary *)newDefinition {
+    [definition release];
+    definition = [[NSDictionary alloc] initWithDictionary:newDefinition];
 }
 
--(void) throwError:(NSString *) errorDescription andBreak:(BOOL) shouldBreak {
-	NSLog(@"%@: %@",[self class],errorDescription);
+-(void) throwError:(NSString *) errorDesc andBreak:(BOOL) shouldBreak {
+   NSLog(@"%@: %@",[self class],errorDesc);
 	if(shouldBreak) {
-		if([delegate respondsToSelector:@selector(breakWithMessage:fromComponent:)]) {
-			[delegate breakWithMessage:errorDescription fromComponent:self];
-		}
+        if([[NSApp delegate] respondsToSelector:@selector(presentError:)]) {
+            [[NSApp delegate] presentError:errorDesc];
+		} else {
+            [NSApp presentError:[NSError errorWithDomain:[self class] code:101 userInfo:nil]];
 	}
 }
 
 @end
+
+#pragma mark Preference Keys
+NSString * const TKComponentTypeKey = @"TKComponentType";
+NSString * const TKComponentNameKey = @"TKComponentName";
+NSString * const TKComponentBundleIdentifierKey = @"TKComponentBundleIdentifier";
+NSString * const TKComponentViewNibNameKey = @"TKComponentViewNibName";
+NSString * const TKComponentPreferencesNibNameKey = @"TKComponentPreferencesNibName";
+NSString * const TKComponentPreferencesFileNameKey = @"TKComponentPreferencesFileName";
+NSString * const TKComponentPreferencesFileTypeKey = @"TKComponentPreferencesFileType";
+
---
+++ TKSubject.m
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////
+//  TKSubject.m
+//  TKUtility
+//  --------------------------------------------------------
+//  Author: Travis Nesland <tnesland@gmail.com>
+//  Created: 8/22/10
+//  Copyright 2010 smoooosh software. All rights reserved.
+/////////////////////////////////////////////////////////////
+#import "TKSubject.h"
+
+@implementation TKSubject
+@synthesize code,session,name,drug,dose;
+@end
